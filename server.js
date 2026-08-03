fa85355b2499b79bf3a21d9e500dc415ed319348
Rahul Biswas <rahulbiswas006677@gmail.com>
2026-08-03 11:00:18 +0530

    chore: initialize project and set up Express server

---
+++ server.js
@@ -0,0 +1,28 @@
+/**
+ * Project Title: Basic To-Do List Application with Categories and Deadlines
+ * Project Code: FS07P1A
+ * Organization: Rixi Lab Technologies
+ * Developer ID: BWU/BCA/23/224
+ */
+
+const express = require('express');
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+
+// Middleware
+app.set('view engine', 'ejs'); 
+app.use(express.urlencoded({ extended: true })); 
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/todoDB')
+  .then(() => console.log('MongoDB Connected successfully!'))
+  .catch(err => console.log('DB Connection Error:', err));
+
+// Mount the Routes
+app.use('/', taskRoutes);
+
+// Start Server
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
