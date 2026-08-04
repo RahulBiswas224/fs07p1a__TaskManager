c811b59d11c0c35454ca33c48d5857b9e9b1788a
Rahul Biswas <rahulbiswas006677@gmail.com>
2026-08-04 18:34:13 +0530

    feat:modifying server for handeling fetch requests

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 
 // Middleware
 app.set('view engine', 'ejs'); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.static('public'));
 
